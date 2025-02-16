1b5469a316be1c6e9e3003b54fc2940bde0c099b
Katayama Hirofumi MZ <katayama.hirofumi.mz@gmail.com>
2025-02-16 10:06:29 +0900

    1.6

--- main.cpp
+++ main.cpp
@@ -82,7 +82,7 @@ LPCTSTR get_text(INT id)
     {
         switch (id)
         {
-        case IDT_VERSION: return TEXT("cmd_play バージョン 1.5 by 片山博文MZ\n");
+        case IDT_VERSION: return TEXT("cmd_play バージョン 1.6 by 片山博文MZ\n");
         case IDT_HELP:
             return
                 TEXT("使い方: cmd_play [オプション] [#n] [文字列1] [文字列2] [文字列3] [文字列4] [文字列5] [文字列6]\n")
@@ -111,7 +111,7 @@ LPCTSTR get_text(INT id)
     {
         switch (id)
         {
-        case IDT_VERSION: return TEXT("cmd_play version 1.5 by katahiromz\n");
+        case IDT_VERSION: return TEXT("cmd_play version 1.6 by katahiromz\n");
         case IDT_HELP:
             return
                 TEXT("Usage: cmd_play [Options] [#n] [string1] [string2] [string3] [string4] [string5] [string6]\n")
